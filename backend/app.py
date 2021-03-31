209a5ed711934e786154ef62ce57d8e978127dad
spartanHarvey <58670974+spartanHarvey@users.noreply.github.com>
2021-03-30 21:07:04 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, redirect, url_for, request
 import mysql.connector
 
 
@@ -34,6 +34,33 @@ def dashboard(admin):
 @app.route("/cart/", methods=['GET','POST'])
 def cart():
     return render_template("cart.html")
-    
+
+@app.route("/checkout/", methods=['GET','POST'])
+def checkout():
+    if request.method == 'POST':
+        customer = {
+            "country":request.form.get('country'),
+            "fname":request.form.get('firstName'),
+            "lname":request.form.get('lastName'),
+            "email":request.form.get('email'),
+            "company":request.form.get('company') if request.form.get('company') else "",
+            "address":request.form.get('address'),
+            "address2":request.form.get('address2') if request.form.get('address2') else "",
+            "city":request.form.get('city'),
+            "state":request.form.get('state'),
+            "zipcode":request.form.get('zipcode'),
+            "phone":request.form.get('phone'),
+            "note":request.form.get('note') if request.form.get('note') else ""
+            
+        }
+
+        query = "INSERT INTO customers (country,fname,lname,email,company,addressLine1,addressLine2,city,state,zipcode,phone,note) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        args = (customer['country'],customer['fname'],customer['lname'],customer['email'],customer['company'],customer['address'],customer['address2'],customer['city'],customer['state'],customer['zipcode'],customer['phone'],customer['note'])
+        mycursor.execute(query, args)
+        db.commit()
+        return redirect(url_for("user"))
+    else:
+        return render_template("checkout.html")
+        
 if __name__ == "__main__":
     app.run(debug=True)
